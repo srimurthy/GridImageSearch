9c8df6a2361d7907f2f0778d9253356182e8dcff
Srinivasa Murthy <srmurthy@yahoo-inc.com>
2015-02-02 01:47:23 -0800

    Initial commit

---
+++ app/src/main/java/com/srimurthy/gridimagesearch/activities/ImageDisplayActivity.java
@@ -0,0 +1,48 @@
+package com.srimurthy.gridimagesearch.activities;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import com.srimurthy.gridimagesearch.R;
+import com.srimurthy.gridimagesearch.models.ImageResult;
+
+
+public class ImageDisplayActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_display);
+        //getActionBar().hide();
+        ImageResult imageResult = getIntent().getParcelableExtra("result");
+        ImageView imageView = (ImageView)findViewById(R.id.ivImageResult);
+        Picasso.with(this).load(imageResult.getFullUrl()).into(imageView);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_image_display, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
